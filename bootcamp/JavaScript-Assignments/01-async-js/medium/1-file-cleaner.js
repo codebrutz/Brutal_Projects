8137f9a9eea831ee7f64fc6369352d45c27fb391
codebrutz <piyushw527@gmail.com>
2026-02-20 23:48:59 +0530

    solved medium

--- bootcamp/JavaScript-Assignments/01-async-js/medium/1-file-cleaner.js
+++ bootcamp/JavaScript-Assignments/01-async-js/medium/1-file-cleaner.js
@@ -11,3 +11,29 @@
 // ```
 // hello world my name is raman
 // ```
+
+const fs = require("fs");
+
+fs.readFile("test.txt", "utf8", (err, data) => {
+  if (err) {
+    console.log("Error reading file:", err);
+    return;
+  }
+  let content = data;
+  const cleanedData = content.replace(/\s+/g, " ").trim();
+
+  fs.writeFile("test.txt", cleanedData, (err) => {
+    if (err) {
+      console.log("Error writing file:", err);
+      return;
+    }
+
+    console.log("File cleaned and updated successfully");
+  });
+});
+fs.readFile("test.txt", "utf-8", (err, data) => {
+  if (err) {
+    console.log("error hogya master");
+  }
+  console.log(data);
+});
