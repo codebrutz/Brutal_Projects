2eec7a01d3b517d04896ac4217ddedbb59cc3af6
codebrutz <piyushw527@gmail.com>
2026-02-20 23:32:07 +0530

    solved easy pproblems

--- bootcamp/JavaScript-Assignments/01-async-js/easy/2-counter.js
+++ bootcamp/JavaScript-Assignments/01-async-js/easy/2-counter.js
@@ -3,3 +3,10 @@
 // Without using setInterval, try to code a counter in Javascript. There is a hint at the bottom of the file if you get stuck.
 
 // (Hint: setTimeout)
+let count = 0;
+function period() {
+  count++;
+  console.log(count);
+  setTimeout(period, 1000);
+}
+setTimeout(period, 1000);
--- bootcamp/JavaScript-Assignments/01-async-js/easy/4-write-to-file.js
+++ bootcamp/JavaScript-Assignments/01-async-js/easy/4-write-to-file.js
@@ -2,3 +2,21 @@
 
 // Using the fs library again, try to write to the contents of a file.
 // You can use the fs library to as a black box, the goal is to understand async tasks.
+
+const fs = require("fs");
+console.log("Start");
+
+fs.writeFile("a.txt", "Hello World", function (err, contents) {
+  if (err) {
+    console.log("Error writing file:", err);
+    return;
+  }
+  console.log("File written successfully");
+});
+
+console.log("End");
+let count = 0;
+for (let i = 0; i < 1000000; i++) {
+  count++;
+}
+console.log(count);
